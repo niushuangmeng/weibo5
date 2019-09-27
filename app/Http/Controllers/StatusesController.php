8afcfbf844b69f408ccf5712015f9a8ffaca263d
niushuangmeng <niushuangmeng@gmail.com>
2019-09-27 06:20:34 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Auth;
 class StatusesController extends Controller
@@ -20,6 +20,14 @@ class StatusesController extends Controller
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
